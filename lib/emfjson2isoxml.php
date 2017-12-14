edda63d8f4b14e1d3e6e85813691c6ebe58fed12
root <root@vmi141395.contaboserver.net>
2017-12-14 21:47:43 +0100

    Added validation folder for metadata testing reports

--- lib/emfjson2isoxml.php
+++ lib/emfjson2isoxml.php
@@ -2,9 +2,9 @@
 /*
 ** PHP SCRIPT TO GENERATE JSON ENCODING FROM XML ENVIRONMENTAL MONITORING FACILTY DATASET
 */
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
+//ini_set('display_errors', 1);
+//ini_set('display_startup_errors', 1);
+//error_reporting(E_ALL);
 ini_set('date.timezone','Europe/Belgrade');
 include_once 'xml2json.php';
 header('Content-Type: text/xml');
