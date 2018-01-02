288d76faab8e45d9b17282c3ca0d5e71d327ae7a
root <root@vmi141395.contaboserver.net>
2018-01-02 15:56:37 +0100

    proposed fix for #3

--- emf2iso.php
+++ emf2iso.php
@@ -5,7 +5,7 @@ ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 $now = date("Y-m-dTh:m:s");
 $url = 'https://data.lter-europe.net/deims/emf/harvest_list';
-$url2 = 'http://klimeto.com/projects/2017/uba/app/lib/emfjson2isoxml.php';
+$urlTrans = $_SERVER['HTTP_REFERER'] . 'lib/emfjson2isoxml.php';
 $xml = simplexml_load_file($url) or die("feed not loading");
 $arr = json_decode(json_encode($xml), true);
 $zwischen_var = $arr["site"];
@@ -18,9 +18,9 @@ for ($x = 0; $x < count($zwischen_var); $x++) {
     $temp_record = $zwischen_var[$x];
 	echo("EXTRACT FROM: ". $temp_record["path"]."<br>");
 	// get current directory
-	$file_name = __DIR__ . "/data/emf2gmd_".$temp_record["UUID"].".xml";
-	$emf2iso_xml_file = file_get_contents($url2 . '?url=' . $temp_record["path"]);
-	echo("TRANSFORM WITH: " . $url2 . "?url=" . $temp_record["path"]."<br>");
+	$file_name = __DIR__ . "/data/emf2iso/emf2gmd_".$temp_record["UUID"].".xml";
+	$emf2iso_xml_file = file_get_contents($urlTrans . '?url=' . $temp_record["path"]);
+	echo("TRANSFORM WITH: " . $urlTrans . "?url=" . $temp_record["path"]."<br>");
 	file_put_contents($file_name, $emf2iso_xml_file);
 	echo("LOAD TO: ".$file_name ."<br>");
 	echo("</li>");
