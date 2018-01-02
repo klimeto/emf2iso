ff33abeedc24a20f49d859fc34d72f53deefdec6
root <root@vmi141395.contaboserver.net>
2018-01-02 16:04:35 +0100

    removed if-clause for #4

--- lib/run_xml2json.php
+++ lib/run_xml2json.php
@@ -8,12 +8,7 @@ error_reporting(E_ALL);
 ini_set('date.timezone','Europe/Belgrade');
 include_once 'xml2json.php';
 header('Content-Type: application/json');
-if (empty($_GET['url'])){
-	$emfXMLUrl = "https://data.lter-europe.net/deims/node/8611/emf";
-}
-else{
-	$emfXMLUrl = $_GET['url'];
-}
+$emfXMLUrl = $_GET['url'];
 $siteEmfArray = xmlToArray(simplexml_load_file($emfXMLUrl));
 $siteEmfJSON = json_encode($siteEmfArray);
 echo $siteEmfJSON;
