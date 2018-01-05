312b1ba648d591dc4c6b225642f6d6f35cdb7dfe
root <root@vmi141395.contaboserver.net>
2018-01-05 13:56:31 +0100

    added validation report

--- emf2iso.php
+++ emf2iso.php
@@ -5,13 +5,14 @@ $xml = simplexml_load_file($url) or die("feed not loading");
 $arr = json_decode(json_encode($xml), true);
 $zwischen_var = $arr["site"];
 $start = microtime(true);
-echo("EMF2ISO START ON " . $start );
+echo("*** EMF2ISO START ON " . $start );
 echo "\r\n";
 echo("RECORDS TO BE PROCESSED: " . count($arr['site']));
 echo "\r\n";
+echo "PROCESSING ... \r\n";
 for ($x = 0; $x < count($zwischen_var); $x++) {
     $temp_record = $zwischen_var[$x];
-	echo("EXTRACT FROM: ". $temp_record["path"]."\r\n");
+	//echo("EXTRACT FROM: ". $temp_record["path"]."\r\n");
 	$file_name = __DIR__ . "/data/emf2iso/emf2gmd_".$temp_record["UUID"].".xml";
 	//$emf2iso_xml_file  = emfXml2isoXml("https://data.lter-europe.net/deims/node/8349/emf");
 	$emf2iso_xml_file = emfXml2isoXml($temp_record["path"]);
@@ -20,13 +21,13 @@ for ($x = 0; $x < count($zwischen_var); $x++) {
 	}
 	else{
 		file_put_contents($file_name, $emf2iso_xml_file);
-		echo("LOADED TO: ".$file_name ."\r\n");
+		//echo("LOADED TO: ".$file_name ."\r\n");
 	}
 }
 $end = microtime(true);
 echo "\r\n";
 echo "\r\n";
-echo("EMF2ISO END ON ".$end);
+echo("*** EMF2ISO END ON ".$end);
 echo "\r\n";
 echo("PROCESS DURATION: " . round(($end - $start),2) . "s");
 echo "\r\n";
--- product2iso.php
+++ product2iso.php
@@ -4,10 +4,11 @@ $url = 'https://data.lter-europe.net/deims/data_product/harvesting_json';
 $json = file_get_contents($url);
 $arr = json_decode($json, true);
 $start = microtime(true);
-echo("PRODUCT2ISO START ON " . $start );
+echo("*** PRODUCT2ISO START ON " . $start );
 echo "\r\n";
 echo("RECORDS TO BE PROCESSED: " . count($arr['nodes']));
 echo "\r\n";
+echo "PROCESSING ... \r\n";
 foreach($arr['nodes'] as $key => $value){
 	$mdURL = $value['data_product']['url'];
 	$mdTitle = $value['data_product']['title'];
@@ -15,8 +16,8 @@ foreach($arr['nodes'] as $key => $value){
 	$getJSON = file_get_contents($mdURL);
 	$mdJSON = json_decode($getJSON);
 	$mdUUID = $mdJSON->{'nodes'}[0]->{'node'}->{'uuid'};
-	echo("EXTRACT FROM: ". $mdURL);
-	echo "\r\n";
+	//echo("EXTRACT FROM: ". $mdURL);
+	//echo "\r\n";
 	$file_name = __DIR__ . "/data/product2iso/product2gmd_".$mdUUID.".xml";
 	//$product2iso_xml_file = productJson2isoXml("https://data.lter-europe.net/deims/node/10170/json");
 	$product2iso_xml_file = productJson2isoXml($mdURL);
@@ -25,15 +26,15 @@ foreach($arr['nodes'] as $key => $value){
 	}
 	else{
 		file_put_contents($file_name, $product2iso_xml_file);
-		echo("LOADED TO: ".$file_name ."\r\n");
+		//echo("LOADED TO: ".$file_name ."\r\n");
 	}
-	echo "\r\n";
+	//echo "\r\n";
 }
 $end = microtime(true);
 echo "\r\n";
 echo "\r\n";
 echo("PRODUCT2ISO END ON ".$end);
 echo "\r\n";
-echo("PROCESS DURATION: " . round(($end - $start),2) . "s");
+echo("*** PROCESS DURATION: " . round(($end - $start),2) . "s");
 echo "\r\n";
 ?>
